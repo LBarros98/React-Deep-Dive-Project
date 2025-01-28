74bc3d55fccdcb0c44d1d006b741df64070a12e3
LBarros98 <lucas.barros.indrusiak@gmail.com>
2025-01-28 10:22:54 -0300

    feat: adding tik-tak-toe project

---
+++ src/components/winnig-combination.js
@@ -0,0 +1,45 @@
+export const WINNING_COMBINATIONS = [
+  // Linhas
+  [
+    { row: 0, col: 0 },
+    { row: 0, col: 1 },
+    { row: 0, col: 2 },
+  ],
+  [
+    { row: 1, col: 0 },
+    { row: 1, col: 1 },
+    { row: 1, col: 2 },
+  ],
+  [
+    { row: 2, col: 0 },
+    { row: 2, col: 1 },
+    { row: 2, col: 2 },
+  ],
+  // Colunas
+  [
+    { row: 0, col: 0 },
+    { row: 1, col: 0 },
+    { row: 2, col: 0 },
+  ],
+  [
+    { row: 0, col: 1 },
+    { row: 1, col: 1 },
+    { row: 2, col: 1 },
+  ],
+  [
+    { row: 0, col: 2 },
+    { row: 1, col: 2 },
+    { row: 2, col: 2 },
+  ],
+  // Diagonais
+  [
+    { row: 0, col: 0 },
+    { row: 1, col: 1 },
+    { row: 2, col: 2 },
+  ],
+  [
+    { row: 0, col: 2 },
+    { row: 1, col: 1 },
+    { row: 2, col: 0 },
+  ],
+];
